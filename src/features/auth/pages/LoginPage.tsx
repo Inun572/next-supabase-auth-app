756acba86ad6354a3d85443077693b0fc4d11e6b
Helmi <helmi@volantis.io>
2025-05-06 09:14:51 +0700

    fix: change redirect from login google

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -77,7 +77,7 @@ const LoginPage = () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/callback`,
       }
     });
   };
